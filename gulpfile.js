f252a2437aaa6bc2797a45dd41b17350f08635ae
YoungSeok-Ahn <veris1975@outlook.com>
2018-05-16 12:09:04 -0500

    Add autoprefixer

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css', () => {
+    gulp.src('./src/css/*.css')
+      .pipe(autoprefixer({
+          browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./dist/css/'))
+  })
