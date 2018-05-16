12c12d3f167395f0ce46c3a52f790e1065a19209
YoungSeok-Ahn <veris1975@outlook.com>
2018-05-16 12:19:09 -0500

    Add clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('css', () => {
     gulp.src('./src/css/*.css')
       .pipe(autoprefixer({
           browsers: ['last 2 versions']
       }))
+      .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(gulp.dest('./dist/css/'))
-  })
+  });
